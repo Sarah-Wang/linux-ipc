b247ffe46b39231f02310837d12d75899aad2d89
Sarah-Wang <yyzxml@outlook.com>
2020-01-13 17:46:14 +0800

    change: operate on sem in loop
    Signed-off-by: Sarah-Wang <yyzxml@outlook.com>

--- sems/posix_sem_unnamed_my.c
+++ sems/posix_sem_unnamed_my.c
@@ -12,46 +12,50 @@
 
 
 static sem_t done_sem;
-static int done;
 
-void *set_num(void)
+void *put_sem(void)
 {
-	sleep(100);
-	done = 1;
-	if (sem_post(&done_sem) == -1) {
-		printf("sem_post failed\n");
-		return (void *)(-1);
+	while (1) {
+		sleep(5);
+		if (sem_post(&done_sem) == -1) {
+			printf("sem_post failed\n");
+			return (void *)(-1);
+		}
+	}
+}
+
+void *get_sem(void)
+{
+	while (1) {
+		printf("before sem_wait\n");
+		if (sem_wait(&done_sem) == -1) {
+			perror("sem_wait");
+			exit(1);
+		}
+		usleep(1000);
+		printf("get sem and do sth...\n");
 	}
 }
 
 int main(int argc, char *argv[])
 {
 	pthread_t tid;
-	int i = 0;
-	time_t now;
-
-	done = 0;
 
 	if (sem_init(&done_sem, 0, 0) == -1) {
 		perror("sem_init");
 		exit(1);
 	}
 
-	pthread_create(&tid, NULL, (void *)set_num, NULL);
+	/* pthread_create(&tid, NULL, (void *)put_sem, NULL); */
+	pthread_create(&tid, NULL, (void *)get_sem, NULL);
 
-	time(&now);
-	printf("line %d time is %ld\n", __LINE__, now);
-	printf("done is: %d\n", done);
-
-	if (sem_wait(&done_sem) == -1) {
-		perror("sem_wait");
-		exit(1);
+	while (1) {
+		sleep(5);
+		if (sem_post(&done_sem) == -1)
+			printf("sem_post failed\n");
+		printf("\n\nafter sem_post\n\n");
 	}
 
-	time(&now);
-	printf("line %d time is %ld\n", __LINE__, now);
-
-	printf("done is: %d\n", done);
 
 	if (sem_destroy(&done_sem) == -1) {
 		perror("sem_destroy");
